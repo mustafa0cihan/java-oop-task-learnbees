8688931d6f3396254e47ee604f3e000ad8dd16dd
mustafa0cihan <mustafa0cihan@gmail.com>
2022-08-30 18:04:22 +0200

    Assignment of Java Code Fix

--- src/main/java/org/example/IManager.java
+++ src/main/java/org/example/IManager.java
@@ -1,6 +1,16 @@
 package org.example;
 
 public interface IManager {
-    // A Manager can increaseSalary, hireEmployee, fireEmployee, and getNumberOfEmployeesSupervised
+    // A Manager can increaseSalary, hireEmployee, fireEmployee, and
+    // getNumberOfEmployeesSupervised
     // Please implement these methods
+
+    public void increaseSalary();
+
+    public void hireEmployee();
+
+    public void fireEmployee();
+
+    public void getNumberOfEmployeesSupervised();
+
 }
--- src/main/java/org/example/SoftwareDeveloper.java
+++ src/main/java/org/example/SoftwareDeveloper.java
@@ -1,7 +1,41 @@
 package org.example;
 
-
-public class SoftwareDeveloper extends Employee implements ISoftwareDeveloper{
+public class SoftwareDeveloper extends Employee implements ISoftwareDeveloper {
     // A Manager has Supervisor, skills, experience attribute additionally.
     // Please implement necessary methods to get and set these attributes of Manager
+    private Manager supervisor;
+    private String skills;
+    private int experience;
+
+    public SoftwareDeveloper(int Id, String FirstName, String LastName, int Age, double Salary, String Department,
+            Manager Supervisor, String Skills, int Experience) {
+        super(Id, FirstName, LastName, Age, Salary, Department);
+        this.supervisor = Supervisor;
+        this.skills = Skills;
+        this.experience = Experience;
+    }
+
+    public void setSupervisor(Manager supervisor) {
+        this.supervisor = supervisor;
+    }
+
+    public void setSkills(String skills) {
+        this.skills = skills;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+
+    public Manager getSupervisor() {
+        return supervisor;
+    }
+
+    public String getSkills() {
+        return skills;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
 }
